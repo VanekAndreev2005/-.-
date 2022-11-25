f7486b26219fcbb6ca783fb17cc44a05af6407b8
VanekAndreev2005 <118421152+VanekAndreev2005@users.noreply.github.com>
2022-11-25 16:29:09 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Абсолютная разность
+            Console.WriteLine("Введите любое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            if (n > 123)
+            {
+                sum = (n - 123) * 3;
+            }
+            else
+            {
+                sum = 123 - n;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();*/
+
+
+        }
+    }
+}
